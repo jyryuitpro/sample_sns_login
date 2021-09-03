779c6396c573ef96314c0432923f22af8858a0fa
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-09-03 12:42:04 +0900

    파이어베이스 SNS 로그인 정복하기 / 애플 로그인 편 [iOS 이하 ] #7

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:uuid/uuid.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
@@ -13,7 +15,7 @@ class Login extends StatelessWidget {
 
     // Obtain the auth details from the request
     final GoogleSignInAuthentication googleAuth =
-    await googleUser!.authentication;
+        await googleUser!.authentication;
 
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
@@ -31,15 +33,17 @@ class Login extends StatelessWidget {
 
     // Create a credential from the access token
     final OAuthCredential facebookAuthCredential =
-    FacebookAuthProvider.credential(loginResult.accessToken!.token);
+        FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
     // Once signed in, return the UserCredential
     return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
   }
 
   Future<UserCredential> signInWithApple() async {
-    // Request credential for the currently signed in Apple account.
-    final appleCredential = await SignInWithApple.getAppleIDCredential(
+    bool isAvailable = await SignInWithApple.isAvailable();
+    if (isAvailable) {
+      // Request credential for the currently signed in Apple account.
+      final appleCredential = await SignInWithApple.getAppleIDCredential(
         scopes: [
           AppleIDAuthorizationScopes.email,
           AppleIDAuthorizationScopes.fullName,
@@ -47,19 +51,41 @@ class Login extends StatelessWidget {
         webAuthenticationOptions: WebAuthenticationOptions(
           clientId: 'com.anomalo.snslogin.sample-sns-login.web',
           redirectUri: Uri.parse(
-              'https://midnight-robust-drop.glitch.me//callbacks/sign_in_with_apple'),)
-        ;
-    );
+              'https://midnight-robust-drop.glitch.me//callbacks/sign_in_with_apple'),
+        ),
+      );
 
-    // Create an `OAuthCredential` from the credential returned by Apple.
-    final oauthCredential = OAuthProvider("apple.com").credential(
-      idToken: appleCredential.identityToken,
-      accessToken: appleCredential.authorizationCode,
-    );
+      // Create an `OAuthCredential` from the credential returned by Apple.
+      final oauthCredential = OAuthProvider("apple.com").credential(
+        idToken: appleCredential.identityToken,
+        accessToken: appleCredential.authorizationCode,
+      );
 
-    // Sign in the user with Firebase. If the nonce we generated earlier does
-    // not match the nonce in `appleCredential.identityToken`, sign in will fail.
-    return await FirebaseAuth.instance.signInWithCredential(oauthCredential);
+      // Sign in the user with Firebase. If the nonce we generated earlier does
+      // not match the nonce in `appleCredential.identityToken`, sign in will fail.
+      return await FirebaseAuth.instance.signInWithCredential(oauthCredential);
+    } else {
+      final clientState = Uuid().v4();
+      final url = Uri.https('appleid.apple.com', '/auth/authorize', {
+        'response_type': 'code id_token',
+        'client_id': "com.anomalo.snslogin.sample-sns-login.web",
+        'response_mode': 'form_post',
+        'redirect_uri':
+            'https://midnight-robust-drop.glitch.me//callbacks/sign_in_with_apple',
+        'scope': 'email name',
+        'state': clientState,
+      });
+
+      final result = await FlutterWebAuth.authenticate(
+          url: url.toString(), callbackUrlScheme: "applink");
+
+      final body = Uri.parse(result).queryParameters;
+      final oauthCredential = OAuthProvider("apple.com").credential(
+        idToken: body['id_token'],
+        accessToken: body['code'],
+      );
+      return await FirebaseAuth.instance.signInWithCredential(oauthCredential);
+    }
   }
 
   @override
