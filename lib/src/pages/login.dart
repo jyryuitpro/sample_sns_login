b3ccf10b4b9b208c7dcb1004a235391abfa93994
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-09-03 12:28:18 +0900

    파이어베이스 SNS 로그인 정복하기 / 애플 로그인 편 [Android] #6

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -13,7 +13,7 @@ class Login extends StatelessWidget {
 
     // Obtain the auth details from the request
     final GoogleSignInAuthentication googleAuth =
-        await googleUser!.authentication;
+    await googleUser!.authentication;
 
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
@@ -31,7 +31,7 @@ class Login extends StatelessWidget {
 
     // Create a credential from the access token
     final OAuthCredential facebookAuthCredential =
-        FacebookAuthProvider.credential(loginResult.accessToken!.token);
+    FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
     // Once signed in, return the UserCredential
     return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
@@ -40,10 +40,15 @@ class Login extends StatelessWidget {
   Future<UserCredential> signInWithApple() async {
     // Request credential for the currently signed in Apple account.
     final appleCredential = await SignInWithApple.getAppleIDCredential(
-      scopes: [
-        AppleIDAuthorizationScopes.email,
-        AppleIDAuthorizationScopes.fullName,
-      ],
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+        webAuthenticationOptions: WebAuthenticationOptions(
+          clientId: 'com.anomalo.snslogin.sample-sns-login.web',
+          redirectUri: Uri.parse(
+              'https://midnight-robust-drop.glitch.me//callbacks/sign_in_with_apple'),)
+        ;
     );
 
     // Create an `OAuthCredential` from the credential returned by Apple.
